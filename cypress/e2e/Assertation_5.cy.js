881c6e4cd93cabb1d2bfc94e1385d5d2f674abf7
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-07-10 08:16:56 +0530

    Assertation of should

--- cypress/e2e/Assertation_5.cy.js
+++ cypress/e2e/Assertation_5.cy.js
@@ -1,8 +1,30 @@
-const { describe } = require("node:test");
+//const { describe } = require("node:test");
 
 describe('Assertation demo',()=>{
 
     it('Implicit Assertation',()=>{
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        //Implicit assertation  >>should , and 
+
+        cy.url().should('include','orangehrmlive.com') // cy.url()>> returns url
+
+        cy.url().should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        
+        cy.url().should('contain','orangehrmlive') // contain and include keywords are almost same function.
+
+
+        //no repeation of cy.url()
+
+        cy.url().should('include','orangehrmlive.com') // cy.url()>> returns url
+
+        .should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        
+        .should('contain','orangehrmlive') // contain and include keywords are almost same function.
+
+
+        
 
     })
 })
