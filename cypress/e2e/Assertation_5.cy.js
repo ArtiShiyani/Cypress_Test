e588f375edd53841f7c550d8b3e93b759a049576
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-07-11 08:20:49 +0530

    implicit assertation

--- cypress/e2e/Assertation_5.cy.js
+++ cypress/e2e/Assertation_5.cy.js
@@ -23,8 +23,28 @@ describe('Assertation demo',()=>{
         
         .should('contain','orangehrmlive') // contain and include keywords are almost same function.
 
+        //Use of shoud and 'and'keyword.
 
+        cy.url().should('include','orangehrmlive.com') // cy.url()>> returns url
+
+        .and('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         
+        .and('contain','orangehrmlive') // contain and include keywords are almost same function.
 
+        .and('not.contain','grennhrmlivr')// not. keyword used for negative assertation.
+
+        //cy.title() returns the  title of current web page
+
+        cy.title().should('include','Orange')
+
+        .and('eq','OrangeHRM')
+
+        .and('contain','HRM')
+
+
+        //Validation on prticular web element
+
+        cy.get('.orangehrm-login-branding > img').should('be.visible') // to check that logo is present or not on webpage
+        .and('exist')
     })
 })
