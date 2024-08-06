d7a4d8d0ba009c934d961664da0819748ad30063
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-08-07 00:28:24 +0530

    Script for different dropdown

--- cypress/e2e/DropDown_8.cy.js
+++ cypress/e2e/DropDown_8.cy.js
@@ -7,20 +7,44 @@ describe('Handle different DropDown',()=>{
 
     cy.visit('https://www.zoho.com/commerce/free-demo.html')
 
-    cy.get('#zcf_address_country').select('Iran').should('have.value','Iran') // Assertation
+    cy.get('#zcf_address_country').select('Iran').should('have.value','Iran') // Assertation >>select tag is there then use "have.value".
 
 
     })
 
-    it('Dropdown without select(tag)',()=>{
+    it.skip('Dropdown without select(tag)',()=>{
 
         cy.visit('https://www.dummyticket.com/dummy-ticket-for-visa-application/')
     
-        cy.get('#select2-billing_country-container').click()
+        cy.get('#select2-billing_country-container').click() // Dropdown locator.
 
         cy.get('.select2-search__field').type('Italy').type('{enter}')// type('{enter}')>>> to press enter.
 
-    
+        //To check entered value is available in dropdown or not.
+
+        cy.get('#select2-billing_country-container').should('have.text','Italy')
     
         })
+
+        it.skip('Auto sugget Dropdown',()=>{
+
+            cy.visit('https://www.wikipedia.org/')
+
+            //cy.xpath('//select[@id="searchLanguage"]').select('हिन्दी').should('have.value','hi')
+        
+            cy.xpath("//input[@id='searchInput']").type('Gandhinagar')
+
+            cy.xpath('//h3[@class="suggestion-title"]').contains('Gandhinagar').click()
+        
+            })
+
+            it('Dynamic Dropdown',()=>{
+
+                cy.visit('')
+    
+                cy.xpath('').type('')
+    
+                cy.xpath('').contains('').click()
+            
+                })
 })
