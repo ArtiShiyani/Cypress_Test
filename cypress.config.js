caf5d7a8f113860fea3f09261a671dcdbdbd7ef8
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-09-01 22:36:57 +0530

    Config chnage

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      screenshotOnRunFailure=true;
+      screenshotOnRunFailure=false;
       require('cypress-mochawesome-reporter/plugin')(on); // for html report
     },
   },
