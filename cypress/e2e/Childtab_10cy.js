7b0576b321b4ea8428a128a1bd8d71c898107237
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-09-04 00:47:31 +0530

    script for child tab approach 1

---
+++ cypress/e2e/Childtab_10cy.js
@@ -0,0 +1,18 @@
+
+describe('Handling tab approach 1',(()=>{
+
+    //by default cypress not supports handling child tab but we have to make work around
+
+
+    it('Approach 1',()=>{
+
+        cy.visit('https://the-internet.herokuapp.com/windows')
+
+        cy.get('.example >a').invoke('removeAttr','target').click() // invoke ()>> used to remoev attribute
+
+        cy.url().should('include','https://the-internet.herokuapp.com/windows/new')
+
+        cy.wait(5000)
+        cy.go('back')
+    })
+}))
