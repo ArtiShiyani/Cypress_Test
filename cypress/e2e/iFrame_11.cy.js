07ab60554b3a8d50b8ef655c7e9d02a6810eedde
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-09-13 00:50:17 +0530

    script for child tab and iframe

---
+++ cypress/e2e/iFrame_11.cy.js
@@ -0,0 +1,14 @@
+//const { describe } = require("node:test");
+
+describe('handling frames',()=>{
+
+    it('',()=>{
+
+        cy.visit('https://the-internet.herokuapp.com/iframe')
+
+
+    })
+
+
+
+})
