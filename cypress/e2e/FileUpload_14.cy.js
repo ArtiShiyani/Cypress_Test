2a4ccd3e6835611e339852af05699a45aecfe2f5
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-12-29 17:37:06 +0530

    File upload

--- cypress/e2e/FileUpload_14.cy.js
+++ cypress/e2e/FileUpload_14.cy.js
@@ -6,7 +6,7 @@ import 'cypress-file-upload' // for file upload
 describe('File Uploads',(()=>{
 
 
-    it.only('Single file upload',()=>{
+    it('Single file upload',()=>{
 
         cy.visit('https://the-internet.herokuapp.com/upload');
 
@@ -23,15 +23,42 @@ describe('File Uploads',(()=>{
 
     it('File upload - Rename',()=>{
 
+
+        cy.visit('https://the-internet.herokuapp.com/upload');
+
+        cy.get('#file-upload').attachFile({filePath:'Arti Shiyani - 2.pdf' , fileName:'myfile.pdf'});
+
+        cy.get('#file-submit').click();
+
+        cy.wait(3000);
+
+        cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+
     })
 
 
     it('File upload -  Drag and Drop',()=>{
 
+        cy.visit('https://the-internet.herokuapp.com/upload');
+
+        cy.get('#drag-drop-upload').attachFile('Arti Shiyani - 2.pdf',{subjectType:'drag-n-drop'});
+
+        cy.wait(5000);
+
+        cy.get('#drag-drop-upload > .dz-preview > .dz-details > .dz-filename > span').contains('rti Shiyani - 2.pdf');
+
+        cy.get('#drag-drop-upload > .dz-preview > .dz-success-mark').should('be.visible');
+
     })
 
 
-    it('Multiple File upload',()=>{
+    it.only('Multiple File upload',()=>{
+
+        cy.visit('https://davidwalsh.name/demo/multiple-file-upload.php')
+
+        cy.get('#filesToUpload').attachFile(['Arti Shiyani - 2.pdf','Arti Shiyani - 2 copy.pdf','Arti Shiyani - 2 copy.pdf']);
+
+        cy.get("ul[id='fileList'] li").contains('Arti Shiyani - 2.pdf');
 
     })
 
