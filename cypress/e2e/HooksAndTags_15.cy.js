179571496996a276c6adcaf5d2b9fd873c0736ea
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-02-20 01:02:51 +0530

    hooks and tags

--- cypress/e2e/HooksAndTags_15.cy.js
+++ cypress/e2e/HooksAndTags_15.cy.js
@@ -4,10 +4,63 @@
 //afterEach
 
 
-describe('',()=>{
+describe('My Suite',()=>{
+
+
+    before(()=>{
+
+        cy.log('***** Launch app *****');
+        
+
+    })
+
+    after(()=>{
+
+        cy.log('***** Close App *****');
+        
+    })
+
+    beforeEach(()=>{
+
+        cy.log('***** Login *****');
+        
+
+    })
+
+    afterEach(()=>{
+
+        cy.log('***** Logout ~ *****');
+        
+
+    })
+
+    it('Search',()=>{
+
+        cy.log('***** Searching *****');
+        
+    })
+
+    it('Advanced saerch',()=>{
+
+        cy.log('*****  Advanced Searching *****');
+        
+        
+    })
+
+    it('Listing product',()=>{
+
+        cy.log('***** Listing product *****');
+        
+        
+    })
 
     it('',()=>{
         
     })
+
+    it('',()=>{
+        
+    })
+
 
 })
